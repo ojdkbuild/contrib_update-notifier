7e4a95de8517e1128b0b93409d9095dd88382395
akashche <akashche@redhat.com>
2016-11-16 07:30:59 -0800

    more syscalls checks in notifier

--- src/notifier.cpp
+++ src/notifier.cpp
@@ -47,7 +47,7 @@ std::wstring load_resource_string(UINT id) {
     }
 }
 
-BOOL load_input_json() {
+bool load_input_json() {
     try {
         std::string exepath = ch::platform::current_executable_path();
         std::string dirpath = ch::utils::strip_filename(exepath);
@@ -66,7 +66,7 @@ BOOL load_input_json() {
     }
 }
 
-BOOL add_notification(HWND hwnd) {
+bool add_notification(HWND hwnd) {
     NOTIFYICONDATA nid;
     memset(&nid, '\0', sizeof(NOTIFYICONDATA));
     nid.cbSize = sizeof(NOTIFYICONDATA);
@@ -74,18 +74,33 @@ BOOL add_notification(HWND hwnd) {
     nid.uFlags = NIF_INFO | NIF_ICON | NIF_TIP | NIF_MESSAGE | NIF_SHOWTIP | NIF_GUID;
     nid.guidItem = __uuidof(NOTIFIER_ICON);
     nid.uCallbackMessage = WMAPP_NOTIFYCALLBACK;
-    LoadIconMetric(NOTIFIER_HANDLE_INSTANCE, MAKEINTRESOURCE(IDI_NOTIFICATIONICON), LIM_SMALL, &nid.hIcon);
-    LoadStringW(NOTIFIER_HANDLE_INSTANCE, IDS_TOOLTIP, nid.szTip, ARRAYSIZE(nid.szTip));
+    HRESULT err_icon = LoadIconMetric(NOTIFIER_HANDLE_INSTANCE, MAKEINTRESOURCE(IDI_NOTIFICATIONICON), LIM_SMALL, &nid.hIcon);
+    if (S_OK != err_icon) {
+        return false;
+    }
+    int err_tooltip = LoadStringW(NOTIFIER_HANDLE_INSTANCE, IDS_TOOLTIP, nid.szTip, ARRAYSIZE(nid.szTip));
+    if (0 == err_tooltip) {
+        return false;
+    }
     nid.dwInfoFlags = NIIF_INFO | NIIF_NOSOUND | NIIF_RESPECT_QUIET_TIME;
-    LoadStringW(NOTIFIER_HANDLE_INSTANCE, IDS_BALLOON_TITLE, nid.szInfoTitle, ARRAYSIZE(nid.szInfoTitle));
-    wcscpy_s(nid.szInfo, ARRAYSIZE(nid.szInfo), NOTIFIER_BALLOON_TEXT.c_str());
-
-    Shell_NotifyIcon(NIM_ADD, &nid);
-    nid.uVersion = NOTIFYICON_VERSION_4;
-    return Shell_NotifyIcon(NIM_SETVERSION, &nid);
+    int err_title = LoadStringW(NOTIFIER_HANDLE_INSTANCE, IDS_BALLOON_TITLE, nid.szInfoTitle, ARRAYSIZE(nid.szInfoTitle));
+    if (0 == err_title) {
+        return false;
+    }
+    errno_t err_info = wcscpy_s(nid.szInfo, ARRAYSIZE(nid.szInfo), NOTIFIER_BALLOON_TEXT.c_str());
+    if (0 != err_info) {
+        return false;
+    }
+    BOOL success = Shell_NotifyIcon(NIM_ADD, &nid);
+    if (success) {
+        nid.uVersion = NOTIFYICON_VERSION_4;
+        return Shell_NotifyIcon(NIM_SETVERSION, &nid);
+    } else {
+        return false;
+    }
 }
 
-BOOL delete_notification() {
+bool delete_notification() {
     NOTIFYICONDATA nid;
     memset(&nid, '\0', sizeof(NOTIFYICONDATA));
     nid.cbSize = sizeof(NOTIFYICONDATA);
@@ -94,7 +109,7 @@ BOOL delete_notification() {
     return Shell_NotifyIcon(NIM_DELETE, &nid);
 }
 
-BOOL restore_tooltip() {
+bool restore_tooltip() {
     NOTIFYICONDATA nid;
     memset(&nid, '\0', sizeof(NOTIFYICONDATA));
     nid.cbSize = sizeof(NOTIFYICONDATA);
@@ -103,42 +118,52 @@ BOOL restore_tooltip() {
     return Shell_NotifyIcon(NIM_MODIFY, &nid);
 }
 
-void show_context_menu(HWND hwnd, int x, int y) {
-    HMENU hMenu = LoadMenu(NOTIFIER_HANDLE_INSTANCE, MAKEINTRESOURCE(IDC_CONTEXTMENU));
-    if (hMenu) {
-        HMENU hSubMenu = GetSubMenu(hMenu, 0);
-        if (hSubMenu) {
-            // our window must be foreground before calling TrackPopupMenu or the menu will not disappear when the user clicks away
-            SetForegroundWindow(hwnd);
-            // respect menu drop alignment
-            UINT uFlags = TPM_RIGHTBUTTON;
-            if (0 != GetSystemMetrics(SM_MENUDROPALIGNMENT)) {
-                uFlags |= TPM_RIGHTALIGN;
-            } else {
-                uFlags |= TPM_LEFTALIGN;
-            }
-            TrackPopupMenuEx(hSubMenu, uFlags, x, y, hwnd, NULL);
-        }
-        DestroyMenu(hMenu);
+bool show_context_menu(HWND hwnd, int x, int y) {
+    HMENU menu = LoadMenu(NOTIFIER_HANDLE_INSTANCE, MAKEINTRESOURCE(IDC_CONTEXTMENU));
+    if (NULL == menu) {
+        return false;
     }
+    HMENU submenu = GetSubMenu(menu, 0);
+    if (NULL == submenu) {
+        return false;
+    }
+    // our window must be foreground before calling TrackPopupMenu or the menu will not disappear when the user clicks away
+    BOOL err_fg = SetForegroundWindow(hwnd);
+    if (0 == err_fg) {
+        return false;
+    }
+    // respect menu drop alignment
+    UINT uFlags = TPM_RIGHTBUTTON;
+    if (0 != GetSystemMetrics(SM_MENUDROPALIGNMENT)) {
+        uFlags |= TPM_RIGHTALIGN;
+    } else {
+        uFlags |= TPM_LEFTALIGN;
+    }
+    BOOL err_track = TrackPopupMenuEx(submenu, uFlags, x, y, hwnd, NULL);
+    if (0 == err_track) {
+        return false;
+    }
+    return DestroyMenu(menu);
 }
 
-void open_browser() {
+bool open_browser() {
     std::wstring url = load_resource_string(IDS_BROWSER_URL);
-    ShellExecuteW(NULL, NULL, url.c_str(), NULL, NULL, SW_SHOW);
+    HINSTANCE res = ShellExecuteW(NULL, NULL, url.c_str(), NULL, NULL, SW_SHOW);
+    return reinterpret_cast<int>(res) > 32;
 }
 
-void show_about_dialog(HWND hwnd) {
+bool show_about_dialog(HWND hwnd) {
     if (STATE_STANDBY != NOTIFIER_STATE) {
-        return;
+        return true;
     }
     NOTIFIER_STATE = STATE_ABOUT;
     std::wstring title = load_resource_string(IDS_ABOUT_TITLE);
     std::wstring header = load_resource_string(IDS_ABOUT_HEADER);
     std::wstring text = load_resource_string(IDS_ABOUT_TEXT);
-    TaskDialog(hwnd, NOTIFIER_HANDLE_INSTANCE, title.c_str(), header.c_str(), text.c_str(),
+    HRESULT res = TaskDialog(hwnd, NOTIFIER_HANDLE_INSTANCE, title.c_str(), header.c_str(), text.c_str(),
             TDCBF_CLOSE_BUTTON, MAKEINTRESOURCE(IDI_NOTIFICATIONICON), NULL);
     NOTIFIER_STATE = STATE_STANDBY;
+    return S_OK == res;
 }
 
 void show_update_dialog(HWND hwnd) {
@@ -148,10 +173,10 @@ void show_update_dialog(HWND hwnd) {
     NOTIFIER_STATE = STATE_UPDATE;
     int chosen = 0;
     std::wstring title = load_resource_string(IDS_UPDATE_TITLE);
-    TaskDialog(hwnd, NOTIFIER_HANDLE_INSTANCE, title.c_str(), NOTIFIER_UPDATE_HEADER.c_str(),
+    HRESULT res = TaskDialog(hwnd, NOTIFIER_HANDLE_INSTANCE, title.c_str(), NOTIFIER_UPDATE_HEADER.c_str(),
             NOTIFIER_UPDATE_TEXT.c_str(), TDCBF_YES_BUTTON | TDCBF_CANCEL_BUTTON,
             MAKEINTRESOURCE(IDI_NOTIFICATIONICON), &chosen);
-    if (IDYES == chosen) {
+    if (S_OK == res && IDYES == chosen) {
         open_browser();
     }
     DestroyWindow(hwnd);
@@ -159,11 +184,12 @@ void show_update_dialog(HWND hwnd) {
 
 LRESULT CALLBACK window_callback(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
-    case WM_CREATE:
-        if (!add_notification(hwnd)) {
-            return -1;
-        }
-        break;
+    case WM_CREATE: {
+            bool success = add_notification(hwnd);
+            if (!success) {
+                return -1;
+            }
+        } break;
     case WM_COMMAND: {
             int const wmId = LOWORD(wParam);
             switch (wmId) {
@@ -171,8 +197,12 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
                 open_browser();
                 DestroyWindow(hwnd);
                 break;
-            case IDM_ABOUT:
-                show_about_dialog(hwnd);
+            case IDM_ABOUT: {
+                    bool success = show_about_dialog(hwnd);
+                    if (!success) {
+                        DestroyWindow(hwnd);
+                    }
+                }
                 break;
             case IDM_CANCEL:
                 DestroyWindow(hwnd);
@@ -180,29 +210,33 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
             default:
                 return DefWindowProc(hwnd, message, wParam, lParam);
             }
-        }
-        break;
-
+        } break;
     case WMAPP_NOTIFYCALLBACK:
         switch (LOWORD(lParam)) {
         case NIN_SELECT:
             show_update_dialog(hwnd);
             break;
-        case NIN_BALLOONTIMEOUT:
-            restore_tooltip();
-            break;
-
-        case NIN_BALLOONUSERCLICK:
-            restore_tooltip();
-            show_update_dialog(hwnd);
-            break;
-
-        case WM_CONTEXTMENU:
-            show_context_menu(hwnd, LOWORD(wParam), HIWORD(wParam));
-            break;
-        }
-        break;
-
+        case NIN_BALLOONTIMEOUT: {
+                bool success = restore_tooltip();
+                if (!success) {
+                    DestroyWindow(hwnd);
+                }
+            } break;
+        case NIN_BALLOONUSERCLICK: {
+                bool success = restore_tooltip();
+                if (success) {
+                    show_update_dialog(hwnd);
+                } else {
+                    DestroyWindow(hwnd);
+                }
+            } break;
+        case WM_CONTEXTMENU: {
+                bool success = show_context_menu(hwnd, LOWORD(wParam), HIWORD(wParam));
+                if (!success) {
+                    DestroyWindow(hwnd);
+                }
+            } break;
+        } break;
     case WM_TIMER:
         break;
     case WM_DESTROY:
@@ -220,8 +254,9 @@ LRESULT CALLBACK window_callback(HWND hwnd, UINT message, WPARAM wParam, LPARAM 
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR /*lpCmdLine*/, int /* nCmdShow */) {
     // fill globals
     NOTIFIER_HANDLE_INSTANCE = hInstance;
-    if (!(load_input_json())) {
-        return 1;
+    bool err_vcheck = load_input_json();
+    if (!err_vcheck) {
+        return 0; // err handling is too coarse here
     }
     // create ui
     WNDCLASSEX wcex;
@@ -231,14 +266,19 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR /*lpCmdLine*/, int /
     wcex.hInstance = NOTIFIER_HANDLE_INSTANCE;
     wcex.hIcon = LoadIcon(NOTIFIER_HANDLE_INSTANCE, MAKEINTRESOURCE(IDI_NOTIFICATIONICON));
     wcex.lpszClassName = NOTIFIER_WINDOW_CLASS.c_str();
-    RegisterClassExW(&wcex);
+    ATOM err_reg = RegisterClassExW(&wcex);
+    if (0 == err_reg) {
+        return 1;
+    }
     HWND hwnd = CreateWindowExW(0, NOTIFIER_WINDOW_CLASS.c_str(), NULL, 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, NULL, NULL);
-    if (hwnd) {
-        MSG msg;
-        while (GetMessage(&msg, NULL, 0, 0)) {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
+    if (NULL == hwnd) {
+        return 1;
+
+    }
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
     return 0;
 }
