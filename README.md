c380ed5c1b44e2cae19f852198911e1766449310
akashche <akashche@redhat.com>
2016-11-20 14:50:38 -0800

    license and readme added

---
+++ README.md
@@ -0,0 +1,19 @@
+Update Notifier application
+===========================
+
+This project contains two utilities:
+
+ - `checker`: CLI application that downloads update descriptor and writes it to `FOLDERID_LocalAppData` directory
+ - `notifier`: GUI application that reads update descriptor and displays its contents as a System Notification
+
+License information
+-------------------
+
+This project is released under the [Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0).
+
+Changelog
+---------
+
+**2016-11-20**
+
+ * initial public version
--- src/notifier.hpp
+++ src/notifier.hpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2016, akashche at redhat.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef UPDATE_CHECKER_NOTIFIER_HPP
+#define	UPDATE_CHECKER_NOTIFIER_HPP
 
 #define IDM_DOWNLOAD 111
 #define IDM_ABOUT 112
@@ -17,3 +35,5 @@
 #define IDS_SHIPPED_VERSION_NUMBER 148
 #define IDS_VENDOR_DIRNAME 149
 #define IDS_APP_DIRNAME 150
+
+#endif // UPDATE_CHECKER_NOTIFIER_HPP
